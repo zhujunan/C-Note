e9986f8f8b3f5a5e450bf83860e9402ed078141d
zhujunan <707620280@qq.com>
2020-08-30 19:47:57 +0800

    Update 02.requests实战之网页采集器.py

--- 爬虫实践/2.requests模块基础/02.requests实战之网页采集器.py
+++ 爬虫实践/2.requests模块基础/02.requests实战之网页采集器.py
@@ -9,16 +9,19 @@
 #UA伪装：让爬虫对应的请求载体身份标识伪装成某一款浏览器
 import requests
 if __name__ == "__main__":
+    
     #UA伪装：将对应的User-Agent封装到一个字典中
     headers = {
         'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
     }
     url = 'https://www.sogou.com/web'
+    
     #处理url携带的参数：封装到字典中
     kw = input('enter a word:')
     param = {
         'query':kw
     }
+    
     #对指定的url发起的请求对应的url是携带参数的，并且请求过程中处理了参数
     response = requests.get(url=url,params=param,headers=headers)
 
